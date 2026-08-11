f84d4e551973bfb31ac89f7b6193e58986bb3adb
Steve Taylor <steve@deployhub.com>
2026-08-11 12:51:05 -0600

    use split pools

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 // Package main is the entrypoint for the cli
 package main
 
-import "relscanner/cmd"
+import (
+	"net/http"
+	"relscanner/cmd"
+	"time"
+)
 
 func main() {
 	// Execute initializes the command structure and executes the root command.
 	// All logic for the Kubernetes Job worker is handled within the cmd package.
 	cmd.Execute()
+}
+
+func init() {
+	http.DefaultClient.Timeout = 120 * time.Second
 }
